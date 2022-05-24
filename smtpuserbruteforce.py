7a63d36e7187698959e9b4f9c653d94e2efe5bf1
Renan Bocchi <bocchi.dev@gmail.com>
2022-05-24 00:09:00 -0400

    Comentando Script e adicionando userlist

--- smtpuserbruteforce.py
+++ smtpuserbruteforce.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys, socket;
 
+#Valida se os argumentos necessarios foram passados
 if len(sys.argv) != 3:
 	print("Modo de uso:\r\n./smtpenum.py <ip> <userlist>")
 else:
-	list = open(sys.argv[2],"r+")
+	list = open(sys.argv[2],"r+") #importa wordlist
 
 	tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	tcp.connect((sys.argv[1],25))
+	tcp.connect((sys.argv[1],25)) #realiza conexao tcp/ip
 
 	banner = tcp.recv(1024)
 
@@ -22,5 +23,5 @@ else:
 
 		exists = tcp.recv(1024)
 
-		if "252" in str(exists):
+		if "252" in str(exists): #printa somente usuarios validos
 			print(exists)
